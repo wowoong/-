f6165afa391d243bc186a6357a757d6932a81a3f
wowoong <hyenyoung92@naver.com>
2021-08-11 18:03:24 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const horizon = document.querySelector('.horizontal');
+const vertical = document.querySelector('.vertical');
+const target = document.querySelector('.target');
+const tag = document.querySelector('.tag');
+
+const targetRect = target.getBoundingClientRect();
+const targetHalfWidth = targetRect.width / 2;
+const targetHalfHeight = targetRect.height / 2;
+
+document.addEventListener('mousemove', (event) => {
+  const x = event.clientX;
+  const y = event.clientY;
+  console.log(`${x}, ${y}`);
+
+  vertical.style.transform = `translateX(${x}px)`;
+  horizon.style.transform = `translateY(${y}px)`;
+  target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+  tag.style.transform = `translate(${x}px, ${y}px)`;
+  tag.innerHTML= `${x}px,${y}px`;
+});
